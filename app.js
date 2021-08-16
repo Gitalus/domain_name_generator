da75f0c0534a30562c7d975d916428c7b0cf92b6
Italo Guajardo <italog.penaloza@gmail.com>
2021-08-16 18:01:06 -0400

    add extensions var

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
 let pronoun = ['the','our', 'my', ''];
 let adj = ['great', 'big', 'small', 'amazing', 'beautiful', 'super', 'last'];
 let noun = ['jogger','racoon'];
-
+let extensions = ['.com', '.net', '.cl', '.io'];
