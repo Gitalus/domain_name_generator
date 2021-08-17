67ffb9fcc6757892ae1295f0bbd64f3c78772f34
Italo Guajardo <italog.penaloza@gmail.com>
2021-08-17 09:15:51 -0400

    finish exercise prototype

--- app.js
+++ app.js
@@ -1,4 +1,21 @@
-let pronoun = ['the','our', 'my', ''];
-let adj = ['greatest', 'big', 'amazing', 'beautiful', 'super', 'last'];
-let noun = ['jogger','racoon', 'dog', 'portal'];
+let pronouns = ['the','our', 'my', ''];
+let adjs = ['greatest', 'amazing', 'super', 'last'];
+let nouns = ['cat', 'dog', 'portal'];
 let extensions = ['.com', '.net', '.cl', '.io'];
+
+
+function domainGenerator() {
+    let domains = [];
+    for (const pronoun of pronouns) {
+        for (const adj of adjs) {
+            for (const noun of nouns) {
+                for (const extension of extensions) {
+                    domains.push(`${pronoun}${adj}${noun}${extension}`)
+                }
+            }
+        }
+    }
+    return domains;
+}
+
+console.log(domainGenerator());
